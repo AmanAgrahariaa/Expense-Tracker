15533c859b801ac9a332e39ee067beb95432304a
AmanAgrahariaa <agraharia329@gmail.com>
2024-01-07 19:55:09 +0530

    ready to launch

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -2,14 +2,49 @@ const mongoose=require('mongoose')
 const colors=require('colors')
 require('dotenv').config();
 
-const connectDb=async()=>{
-mongoose.connect(
-    process.env.MONGOURI
-)
-.then(() => console.log('DB Connected Successfull'))
-.catch((err) => {
-    console.error(err);
-});
-}
+// const connectDb=async()=>{
+    
+// mongoose.connect(
+//     process.env.MONGOURI
+// )
+// .then(() => console.log('DB Connected Successfull'))
+// .catch((err) => {
+//     console.log("error connecting database")
+//     console.error(err);
+// });
+// }
+
+// connectDb();
+
+
+
+const connectDb = async () => {
+    try{
+        console.log(process.env.MONGOURI);
+        await mongoose.connect(process.env.MONGOURI);
+        console.log('DB Connected Successfully');
+    }
+    catch(err) {
+        console.log("error connecting database");
+        console.error(err);
+        process.exit(0);
+    }
+};
 
 connectDb();
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// const MONGOURI = process.env.MONGOURI;
+// console.log(MONGOURI);
+
+// const Connection = async () => {
+//   mongoose.set("strictQuery", true);
+//   mongoose
+//     .connect(MONGOURI, {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//     })
+//     .then(() => console.log("connection successfull .."))
+//     .catch((err) => console.log("error is ----- >>>>> ------ \n", err));
+// };
+// Connection();
--- cllient/src/services.js
+++ cllient/src/services.js
@@ -1,1 +1,2 @@
-export const backend_url = 'http://localhost:8080';
+// export const backend_url = 'http://localhost:8080';
+export const backend_url = 'https://backend-expense-tracker-b4mm.onrender.com'
